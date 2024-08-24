4f4a9e0a1e821cd0a45e1cec5810df15b51783ef
Sandhya <sandhya7am@gmail.com>
2024-08-25 00:00:13 +0530

    This is my first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const mobile = document.querySelector('menu-toggle');
+const mobileLink = document.querySelector('.sidebar');
+mobile.addEventListener("click", function(){
+    mobile.classList.toggle("is.active");
+    mobileLink.classList.toggle("active");
+})
